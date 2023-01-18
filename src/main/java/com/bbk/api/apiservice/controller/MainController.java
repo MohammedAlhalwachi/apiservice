eb17de5350f4dfa95fff9e06638cc3eefff232dc
Mohammed Alhalwachi <mohammed.abdameeralhalwachi@gmail.com>
2023-01-18 11:28:55 +0300

    last

---
+++ src/main/java/com/bbk/api/apiservice/controller/MainController.java
@@ -0,0 +1,12 @@
+package com.bbk.api.apiservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @GetMapping("/")
+    String version() {
+        return "Hello World!";
+    }
+}
